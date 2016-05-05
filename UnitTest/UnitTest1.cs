afbe3eb7552f456f115fe3d2ad8cfcafc0504483
blqw <fttl_398@126.com>
2016-05-06 00:10:25 +0800

    uml画了一半

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void MyTestMethod()
+        {
+            
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
